c24e2058f527ab53d5644f79206b685ccb2cc78b
jborges97 <julionastasi@gmail.com>
2023-03-28 22:37:57 -0300

    Order, Instant, ISO 8601 - Parte 1

---
+++ src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
@@ -0,0 +1,16 @@
+package com.educandoweb.course.entities.enums;
+
+public enum OrderStatus {
+	;
+
+	public static OrderStatus valueOf(Integer orderStatus) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
