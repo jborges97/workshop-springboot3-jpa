addbf8fb8a4fd344c48c8e2e917e45d56291cde2
jborges97 <julionastasi@gmail.com>
2023-04-04 21:18:59 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
+++ src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
@@ -1,16 +1,35 @@
 package com.educandoweb.course.entities.enums;
 
 public enum OrderStatus {
-	;
-
-	public static OrderStatus valueOf(Integer orderStatus) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
 	}
 
-	public Integer getCode() {
-		// TODO Auto-generated method stub
-		return null;
+	public int getCode() {
+		return code;
 	}
+
+	public static OrderStatus valueOf(int code) throws IllegalAccessException {
+		for (OrderStatus value : OrderStatus.values()) {
+			if(value.getCode() == code) {
+				return value;
+			}
+		}
+	
+		throw new IllegalAccessException("Invalid OrderStatus code");
+		
+	}
+	
+	
 
 }
+
